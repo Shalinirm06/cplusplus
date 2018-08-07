399fdd19fe8d1a84dd1a702a79fc44c63610ff69
Shalinirm06 <42173000+Shalinirm06@users.noreply.github.com>
2018-08-07 21:08:14 +0530

    Add files via upload

---
+++ ideone_tQqA3A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a ;
+	cin>>a;
+	if(a>0)
+		cout<<"POSITIVE";
+	else if (a<0)
+		cout<<"NEGATIVE";
+		else
+		cout<<"ZERO";
+	return 0;
+}
